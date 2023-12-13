8e240dd56a833d87c12826cbfce134e77a6fe792
Hohyun.Kim <hohyun1022@naver.com>
2023-12-13 16:08:28 +0900

    feat: add Register

--- frontend/src/components/atoms/login/Input.jsx
+++ frontend/src/components/atoms/login/Input.jsx
@@ -4,11 +4,12 @@ export default function Input({ type, placeholder }) {
 }
 
 const StyledInput = styled.input`
-  background: rgba(255, 255, 255, 0.15);
+  background: #17d0a5;
   box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
   border-radius: 2rem;
   width: 80%;
   height: 3rem;
+  margin: 0.5rem;
   padding: 1rem;
   border: none;
   outline: none;
@@ -22,7 +23,7 @@ const StyledInput = styled.input`
     border-radius: 2rem;
   }
   &::placeholder {
-    color: #b9abe099;
+    color: #04010d99;
     font-weight: 100;
     font-size: 1rem;
   }
