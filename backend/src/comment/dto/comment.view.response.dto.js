61330f961fe98ee544a2cd856eb51b9a13fce921
chapman-blockchain <limth1312@naver.com>
2023-12-18 10:28:19 +0900

    feat: before pull request 231218

--- backend/src/comment/dto/comment.view.response.dto.js
+++ backend/src/comment/dto/comment.view.response.dto.js
@@ -1,42 +1,45 @@
 const BaseDTO = require("../../lib/base.dto");
 
 class CommentViewResponseDTO extends BaseDTO {
-    Comments_uid;
-    Comments_content;
-    Comments_created_at;
-    Boards_id;
-    Users_uid;
-    UserNickname;
-    UserProfile;
-    Replies = [];
+  Comments_uid;
+  Comments_content;
+  Comments_created_at;
+  Boards_id;
+  Users_uid;
+  UserNickname;
+  UserProfile;
+  Replies = [];
 
-    constructor(comment) {
-        super();
+  constructor(comment) {
+    super();
 
-        this.Comments_uid = comment.dataValues.Comments_uid;
-        this.Comments_content = comment.dataValues.Comments_content;
-        this.Comments_created_at = comment.dataValues.Comments_created_at;
-        this.Boards_id = comment.dataValues.Boards_id;
-        this.Users_uid = comment.dataValues.Users_uid;
-        this.UserNickname = comment.dataValues.UserNickname;
-        this.UserProfile = comment.dataValues.UserProfile;
+    this.Comments_uid = comment.dataValues.Comments_uid;
+    this.Comments_content = comment.dataValues.Comments_content;
+    this.Comments_created_at = comment.dataValues.Comments_created_at;
+    this.Boards_id = comment.dataValues.Boards_id;
+    this.Users_uid = comment.dataValues.Users_uid;
+    this.UserNickname = comment.dataValues.UserNickname;
+    this.UserProfile = comment.dataValues.UserProfile;
 
-        if (comment.dataValues.Replies && Array.isArray(comment.dataValues.Replies)) {
-            this.Replies = comment.dataValues.Replies.map((reply) => {
-                return {
-                    Comments_uid: reply.dataValues.Comments_uid,
-                    Comments_content: reply.dataValues.Comments_content,
-                    Comments_created_at: reply.dataValues.Comments_created_at,
-                    UserNickname: reply.dataValues.User.nickname,
-                    UserProfile: reply.dataValues.User.profile,
-                };
-            });
-        }
-
-        this.validate(this);
+    if (
+      comment.dataValues.Replies &&
+      Array.isArray(comment.dataValues.Replies)
+    ) {
+      this.Replies = comment.dataValues.Replies.map((reply) => {
+        return {
+          Comments_uid: reply.dataValues.Comments_uid,
+          Comments_content: reply.dataValues.Comments_content,
+          Comments_created_at: reply.dataValues.Comments_created_at,
+          UserNickname: reply.dataValues.User.nickname,
+          UserProfile: reply.dataValues.User.profile,
+        };
+      });
     }
+
+    this.validate(this);
+  }
 }
 
 module.exports = {
-    CommentViewResponseDTO,
+  CommentViewResponseDTO,
 };
