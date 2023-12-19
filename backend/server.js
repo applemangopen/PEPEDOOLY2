1a85c7ad2c485858a4d0007944eb5172b94bd49f
seon <flowermansk@naver.com>
2023-12-19 14:06:52 +0900

    fix : comments fix conflict2

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const db = require("./src/lib/db");
 const app = require("./app");
 
-app.listen(5000, async () => {
+app.listen(4000, async () => {
     try {
         await db.sequelize.sync({ force: false });
         console.log("Backend Start");
